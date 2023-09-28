c0d1d567390ab17b26c4b282b181b602e614ea0b
gonchip <136641475+gonchip@users.noreply.github.com>
2023-09-28 03:29:16 +0000

    Agregar delete

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,24 +1,86 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCircleXmark } from "@fortawesome/free-regular-svg-icons";
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
+import { faTasks } from "@fortawesome/free-solid-svg-icons";
 
 //create your first component
 const Home = () => {
+	const [inputValue, setInputValue] = useState("")
+	const [todos, setTodos] = useState([])
+
+	const getTask = async () => {
+		try {
+		  let response = await fetch("https://playground.4geeks.com/apis/fake/todos/user/gonchip");
+		  let data = await response.json()
+
+		  if (response.ok) {
+			setTodos(data)
+		  } 
+
+		  console.log(response)
+
+		} catch (error) {
+		  console.log(error)
+		}
+	  };
+
+	  const addTask = async (data) => {
+		try {
+			let response = await fetch("https://playground.4geeks.com/apis/fake/todos/user/gonchip",{
+				method:"PUT",
+				headers:{
+					"Content-Type":"application/json"
+				},
+				body:JSON.stringify(data)
+			})
+
+			if (response.ok) {
+				getTask()
+			}
+
+		} catch (error) {
+			
+		}
+	  }
+
+	  const deleteTask = (id) => {
+		const newTask = todos.filter((t, currentIndex) => id != currentIndex)
+		addTask(newTask)
+	  }
+
+	  useEffect(() => {
+		getTask();
+	  }, []);
+
 	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<div className="container">
+			<h1>My Todos</h1>
+			<ul>
+				<li>
+					<input type="text"
+						placeholder="Que tienes que hacer?"
+						onChange={(e) => setInputValue(e.target.value)}
+						value={inputValue}
+						onKeyPress={(e) => {
+							if (e.key === "Enter") {
+								const Todos = [...todos,{label:inputValue, done:false}]
+								addTask(Todos)
+								setInputValue("")
+							}
+						}} />
+				</li>
+				{todos.map((item, index) => (
+					<li> {item.label} <FontAwesomeIcon icon={faCircleXmark}
+					className="red-mark" onClick={() => deleteTask(index)} /> </li>
+				))}
+
+              <li className="num-tareas">{todos.length} tareas pendientes</li>
+
+			</ul>
+			
 		</div>
 	);
 };
