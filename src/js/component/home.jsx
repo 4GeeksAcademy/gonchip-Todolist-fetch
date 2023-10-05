80d22d40ceba730206e8c5dd874d69bc18d398c7
gonchip <136641475+gonchip@users.noreply.github.com>
2023-10-05 02:38:16 +0000

    Todolist corregido

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -2,133 +2,133 @@ import React, { useEffect, useState } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faCircleXmark } from "@fortawesome/free-regular-svg-icons";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
-import { faTasks } from "@fortawesome/free-solid-svg-icons";
-
-//create your first component
 const Home = () => {
-	const [inputValue, setInputValue] = useState("")
-	const [todos, setTodos] = useState([])
+  const [inputValue, setInputValue] = useState("");
+  const [todos, setTodos] = useState([]);
 
-	const getTask = async () => {
-		try {
-		  let response = await fetch("https://playground.4geeks.com/apis/fake/todos/user/gonchip")
-		  let data = await response.json()
+  const getTask = async () => {
+    try {
+      let response = await fetch(
+        "https://playground.4geeks.com/apis/fake/todos/user/gonchip"
+      );
+      let data = await response.json();
 
-		   if (response.ok) {
-		 	setTodos(data)
-		   } 
+      if (response.ok) {
+        setTodos(data.todos || []); 
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
-		  if (response.status == 404){
-			createUser()
-		  }
+  const createUser = async () => {
+    try {
+      const data = { todos: [] };
+      let response = await fetch(
+        "https://playground.4geeks.com/apis/fake/todos/user/gonchip",
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify(data),
+        }
+      );
 
-		} catch (error) {
-		  console.log(error)
-		}
-	  };
-
-	  const createUser = async () => {
-		try {
-			let response = await fetch("https://playground.4geeks.com/apis/fake/todos/user/gonchip",{
-				method:"POST",
-				headers:{
-					"Content-Type":"application/json"
-				},
-				body:JSON.stringify(data)
-			})
-
-			if (response.ok){
-				getTask()
-			}
-		} catch (error) {
-			console.log(error)
-		}
+      if (response.ok) {
+		getTask();
+	  } else {
 	  }
+	} catch (error) {
 
-	  const addTask = async (data) => {
-		try {
-			let response = await fetch("https://playground.4geeks.com/apis/fake/todos/user/gonchip",{
-				method:"PUT",
-				headers:{
-					"Content-Type":"application/json"
-				},
-				body:JSON.stringify(data)
-			})
+	}
+  };
 
-			if (response.ok) {
-				getTask()
-			}
+  const addTask = async (data) => {
+    try {
+      let response = await fetch(
+        "https://playground.4geeks.com/apis/fake/todos/user/gonchip",
+        {
+          method: "PUT",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({ todos: data }),
+        }
+      );
 
-		} catch (error) {
-			console.error(error)
-		}
-	  }
+      if (response.ok) {
+        getTask();
+      }
+    } catch (error) {
+      console.error(error);
+    }
+  };
 
-	  const deleteTask = (id) => {
-		const newTask = todos.filter((t, currentIndex) => id != currentIndex)
-		addTask(newTask)
-	  }
+  const deleteTask = (id) => {
+    const newTask = todos.filter((t, currentIndex) => id !== currentIndex);
+    addTask(newTask);
+  };
 
-	  async function deleteAll() {
-		try {
-		  let response = await fetch(
-			"https://playground.4geeks.com/apis/fake/todos/user/gonchip",
-			{
-			  method: "DELETE",
-			  headers: {
-				"Content-Type": "application/json",
-			  },
-			}
-		  );
-	  
-		  if (response.ok) {
-			// Después de borrar todas las tareas, crea el usuario nuevamente
-			await createUser()
-			setTodos([])
-		  }
-		} catch (error) {
-		  console.error(error)
-		}
-	  }
+  async function deleteAll() {
+    try {
+      let response = await fetch(
+        "https://playground.4geeks.com/apis/fake/todos/user/gonchip",
+        {
+          method: "PUT",
+          headers: {
+            "Content-Type": "application/json",
+          },
+        }
+      );
 
+      if (response.status == 400) {
+        setTodos([]);
+      }
+    } catch (error) {
+      console.error(error);
+    }
+  }
 
+  useEffect(() => {
+    getTask();
+  }, []);
 
-	  useEffect(() => {
-		getTask()
-	  }, []);
+  return (
+    <div className="container">
+      <h1>My Todos</h1>
+      <ul>
+        <li>
+          <input
+            type="text"
+            placeholder="¿Qué tienes que hacer?"
+            onChange={(e) => setInputValue(e.target.value)}
+            value={inputValue}
+            onKeyPress={(e) => {
+              if (e.key === "Enter") {
+                const newTodos = [...todos, { label: inputValue, done: false }];
+                addTask(newTodos);
+              }
+            }}
+          />
+        </li>
+        {todos.map((item, index) => (
+          <li key={index}>
+            {item.label}
+            <FontAwesomeIcon
+              icon={faCircleXmark}
+              className="red-mark"
+              onClick={() => deleteTask(index)}
+            />
+          </li>
+        ))}
 
-	return (
-		<div className="container">
-			<h1>My Todos</h1>
-			<ul>
-				<li>
-					<input type="text"
-						placeholder="Que tienes que hacer?"
-						onChange={(e) => setInputValue(e.target.value)}
-						value={inputValue}
-						onKeyPress={(e) => {
-							if (e.key === "Enter") {
-								const Todos = [...todos,{label:inputValue, done:false}]
-								addTask(Todos)
-								setInputValue("")
-							}
-						}} />
-				</li>
-				{todos.map((item, index) => (
-					<li> {item.label} <FontAwesomeIcon icon={faCircleXmark}
-					className="red-mark" onClick={() => deleteTask(index)} /> </li>
-				))}
+        <li className="num-tareas">{todos.length} tareas pendientes</li>
+      </ul>
 
-              <li className="num-tareas">{todos.length} tareas pendientes</li>
-
-			</ul>
-
-			<button onClick={deleteAll}>Limpiar todas las tareas</button>
-			
-		</div>
-	);
+      <button onClick={deleteAll}>Limpiar todas las tareas</button>
+    </div>
+  );
 };
 
 export default Home;
